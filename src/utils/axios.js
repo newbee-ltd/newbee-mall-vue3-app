e8f77976d27cf5a91186585ac67345159cb749d1
Nick930826 <xianyou1993@qq.com>
2021-03-22 16:52:58 +0800

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 import { Toast } from 'vant'
 import router from '../router'
 
-axios.defaults.baseURL = process.env.NODE_ENV == 'development' ? '//47.99.134.126:28019/api/v1' : '//47.99.134.126:28019/api/v1'
+axios.defaults.baseURL = process.env.NODE_ENV == 'development' ? '//backend-api-01.newbee.ltd/api/v1' : '//backend-api-01.newbee.ltd/api/v1'
 axios.defaults.withCredentials = true
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers['token'] = localStorage.getItem('token') || ''
