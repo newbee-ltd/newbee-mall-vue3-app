ef9d6c3523e46376915688e87a0341619c2bc6e1
ASCll <hzxbpld@163.com>
2021-06-21 11:38:01 +0800

    fix: 简化首页代码

--- src/App.vue
+++ src/App.vue
@@ -9,92 +9,91 @@
 -->
 
 <template>
-  <div id="app">
-    <router-view class="router-view" v-slot="{ Component }">
-      <transition :name="transitionName">
-        <component :is="Component" />
-      </transition>
-    </router-view>
-  </div>
+  <router-view class="router-view" v-slot="{ Component }">
+    <transition :name="transitionName">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
-import { useRouter } from 'vue-router'
-  export default {
-    setup() {
-      const router = useRouter()
-      const state = reactive({
-        transitionName: 'slide-left'
-      })
-      router.beforeEach((to, from) => {
-        if (to.meta.index > from.meta.index) {
-          state.transitionName = 'slide-left' // 向左滑动
-        } else if (to.meta.index < from.meta.index) {
-          // 由次级到主级
-          state.transitionName = 'slide-right'
-        } else {
-          state.transitionName = ''   // 同级无过渡效果
-        }
-      })
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-      return {
-        ...toRefs(state)
+export default {
+  setup() {
+    const router = useRouter();
+    const transitionName = ref("slide-left");
+
+    router.beforeEach((to, from) => {
+      if (to.meta.index > from.meta.index) {
+        transitionName.value = "slide-left"; // 向左滑动
+      } else if (to.meta.index < from.meta.index) {
+        // 由次级到主级
+        transitionName.value = "slide-right";
+      } else {
+        transitionName.value = ""; // 同级无过渡效果
       }
-    }
-  }
+    });
+
+    return {
+      transitionName,
+    };
+  },
+};
 </script>
 
 <style lang="less">
-html, body {
+html,
+body {
   height: 100%;
   overflow-x: hidden;
   overflow-y: scroll;
 }
 #app {
   height: 100%;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
   color: #2c3e50;
 }
 
-.router-view{
-    width: 100%;
-    height: auto;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    margin: 0 auto;
-    -webkit-overflow-scrolling: touch;
+.router-view {
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: 0 auto;
+  -webkit-overflow-scrolling: touch;
 }
 
 .slide-right-enter-active,
 .slide-right-leave-active,
 .slide-left-enter-active,
-.slide-left-leave-active{
-    height: 100%;
-    will-change: transform;
-    transition: all 500ms;
-    position: absolute;
-    backface-visibility: hidden;
+.slide-left-leave-active {
+  height: 100%;
+  will-change: transform;
+  transition: all 500ms;
+  position: absolute;
+  backface-visibility: hidden;
 }
-.slide-right-enter{
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
 }
-.slide-right-leave-active{
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
 }
-.slide-left-enter{
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
 }
-.slide-left-leave-active{
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
+.slide-left-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
 }
 
 .van-badge--fixed {
