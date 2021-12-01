6db24e02cf12d96f020e5f46bb35aa95973158e3
Nick930826 <xianyou1993@qq.com>
2021-12-01 15:16:27 +0800

    去除router-link的tag属性

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,19 +11,19 @@
 <template>
   <div class="nav-bar van-hairline--top">
     <ul class="nav-list">
-      <router-link tag="li" class="nav-list-item active" to="home">
+      <router-link  class="nav-list-item active" to="home">
         <i class="nbicon nblvsefenkaicankaoxianban-1"></i>
         <span>首页</span>
       </router-link>
-      <router-link tag="li" class="nav-list-item" to="category">
+      <router-link  class="nav-list-item" to="category">
         <i class="nbicon nbfenlei"></i>
         <span>分类</span>
       </router-link>
-      <router-link tag="li" class="nav-list-item" to="cart">
+      <router-link  class="nav-list-item" to="cart">
         <van-icon  name="shopping-cart-o" :badge="!count ? '' : count" />
         <span>购物车</span>
       </router-link>
-      <router-link tag="li" class="nav-list-item" to="user">
+      <router-link  class="nav-list-item" to="user">
         <i class="nbicon nblvsefenkaicankaoxianban-"></i>
         <span>我的</span>
       </router-link>
